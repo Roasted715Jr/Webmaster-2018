a22ef78b5ae6f04f707df44c897e728c69d4ef13
Matthew Duphily <matthew.duphily@live.com>
2017-11-16 21:16:18 -0500

    Started working on JS functionality

---
+++ main.js
@@ -0,0 +1,7 @@
+//Webmaster 2018
+//Use $('.blah').method(); for classes and $('#blach').method(); for ids
+$(document).ready(function () {
+	$(".navBar").click(function () {
+		$(this).hide();
+	});
+});
