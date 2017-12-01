3d348ea816c41b6e8b659e9b4f468e03af939853
Matthew Duphily <matthew.duphily@live.com>
2017-12-01 17:35:48 -0500

    Added nav bar, favicon, and random logooooooo

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
 $(document).ready(() => {
 	//Arrow functions does not contain a default value for the variable 'this', and are best-suited for non-method functions
+	
+	//Store jQuery object as const
+	//const $object = $('nav');
+	
+	let imgNum = Math.ceil(Math.random() * 8);
+	let src = 'img/logo0' + imgNum + '.png';
+	$('#logo').attr('src', src);
+	
+	/*
+	
+	int number;
+	string source;
+	number = random.range(1,8)
+	source = 'img/logo0' + number + '.png';
+	
+	*/
+	
     $("p").click(function(){
         $(this).hide();
     });
-	
-	//Store jQuery object as const
-	const $object = $('nav');
 });
