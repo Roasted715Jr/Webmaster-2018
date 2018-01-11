a5116fa70c8dc1731e8298f3b037408319c1e222
Roasted715Jr <matthew.duphily@live.com>
2018-01-11 15:45:03 -0500

    Started changing alternate layout

--- Alternate Layout/main.js
+++ Alternate Layout/main.js
@@ -7,12 +7,11 @@ $(document).ready(() => {
 	const $body = $('#body');
 	const $logo = $('#logo');
 	const $logoSVG = $('#logoSVG');
-	const $menuBar = $('#menu-list');
-	const $menuOpen = $('#menu-btn');
-	const $menuClose = $('#close-btn');
-	let isMenuHidden = true;
+	const $menuBtn = $('#menu-btn');
+	const $navBar = $('.top-nav');
 
 	//Initialization...
+	$navBar.hide();
 	//Math.random() returns a number between 0 (inclusive) and 1 (exclusive),
 	// meaning 0 is a possible number for Math.random() to return
 	const imgNum = Math.ceil(Math.random() * 8);
@@ -24,24 +23,8 @@ $(document).ready(() => {
 	$logo.attr('src', src);
 	$logo.attr('width', $window.width() / 13);
 	$logo.attr('height', 'auto');
-
-	$menuBar.hide();
-
-	$menuOpen.on('click', () => {
-		ChangeMenuStatus();
+	
+	$menuBtn.on('click', () => {
+		$navBar.slideToggle();
 	});
-	$menuClose.on('click', () => {
-		ChangeMenuStatus();
-	});
-
-	function ChangeMenuStatus () {
-		isMenuHidden = !isMenuHidden;
-		if (isMenuHidden) {
-			$menuBar.css('right', '-325px');
-			window.setTimeout(() => { $menuBar.hide(); }, 500);
-		} else {
-			$menuBar.show();
-			$menuBar.css('right', '0px');
-		}
-	}
 });
