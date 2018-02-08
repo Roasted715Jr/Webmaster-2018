bdae6a8f272d606a9b919c9f1bf4984ef710948f
Roasted715Jr <matthew.duphily@live.com>
2018-02-08 15:38:58 -0500

    Started working on the page template

--- main.js
+++ main.js
@@ -9,19 +9,29 @@ $(document).ready(() => {
 	const $body = $('#body');
 	const $logo = $('#logo');
 	const $logoSVG = $('#logoSVG');
+	let isTemplate = false;
 	
 	//$body.css();
 	
 	//Math.random() returns a number between 0 (inclusive) and 1 (exclusive),
 	// meaning 0 is a possible number for Math.random() to return
 	const imgNum = Math.ceil(Math.random() * 8);
-	let src = ''
-	if (src === '') {
-		src = 'img/logo01.svg';
-	}
 	
-	src = 'img/logo0' + imgNum + '.svg';
-	$logo.attr('src', src);
-	$logo.attr('width', $window.width() / 13);
-	$logo.attr('height', 'auto');
+	let src = '';
+	function refreshPicture() {
+		if(!isTemplate) {
+			src = 'img/logo0' + imgNum + '.svg';
+		} else if (isTemplate) {
+			src = '../img/logo0' + imgNum + '.svg';
+		}
+		$logo.attr('src', src);
+		$logo.attr('width', $window.width() / 13);
+		$logo.attr('height', 'auto');
+	}
+	refreshPicture();
+	
+	
+	$window.on('resize', () => {
+		refreshPicture();
+	});
 });
